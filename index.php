d214dbc2435ce50409273e46bd786de15ccdcd61
Dalero <33146293+Dalero@users.noreply.github.com>
2017-10-27 09:17:11 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<iframe class="button-api-frame" src="https://youtubemp3api.com/@api/button/mp3/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+
+<!-- Optional script that automatically makes iframe content responsive. -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
+<script>iFrameResize({}, '.button-api-frame');</script>
